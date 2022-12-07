013d2fefecb6abc7d4c17eda9187251d10d2042d
tizhen <tizhen@mail.ru>
2022-12-08 00:24:50 +0300

    init

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Число: {number} - палиндром");
+    }
+    else Console.WriteLine($"Число: {number} - НЕ палиндром");
+}
+
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
+}
+else Console.WriteLine($"Некорректный ввод, число не пятизначное");
